0a4e73f820c233d500ed27883f33ad5efd5476b3
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-09-09 18:52:09 +0300

    getAllComments in comment controller

--- backend/routers/controllers/comments.js
+++ backend/routers/controllers/comments.js
@@ -3,6 +3,27 @@ const Comment =require("../../db/models/comments");
 
 const getAllComments = (req, res) => {
 
+  Comment.find({})
+    .then((result) => {
+      if (!result.length) {
+        return res.status(404).json({
+          success: false,
+          message: "No comments yet",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "all the comments",
+        posts: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "server error",
+        err: err,
+      });
+    });
 
 }
 
